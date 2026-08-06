ec123117a3645ec9991e86c7aa81c3695e2fd3e7
Phil Brooks <philbrooksjazz@gmail.com>
2026-08-06 13:02:59 -0700

    correct bug each daily save must have unique fname

--- dsize.sh
+++ dsize.sh
@@ -1,13 +1,25 @@
 #/bin/bash
 
+HDIR=pmb
+
 # backup old data
-cd /home/rock
-cp /home/rock/t_data* /home/rock/archive
-fpath="/home/rock/t_data*"
+cd /home/${HDIR}
+fpath="/home/${HDIR}/t_data*"
+echo $fpath
 fname="${fpath##*/}"
 
 echo $fname
-echo $fname
+
+idx=$(<data_index)
+echo $idx
+echo $fname > nfilename
+nf2=$(<nfilename)
+echo $nf2
+nfile3=${nf2}.${idx}
+echo $nfile3
+cp /home/${HDIR}/$fname /home/${HDIR}/archive/$nfile3
+((idx++))
+echo $idx>data_index
 
 # truncate data file
 
